c8cde5d42c8930fb49829aa6297bf7d1df0b8302
jesus <jibanez@tacitknowledge.com>
2018-03-05 13:11:46 -0600

    at directiva de atributo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBdLnWLRYB1uOs1ajfu3XpBN6CcNKW2X5g'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
