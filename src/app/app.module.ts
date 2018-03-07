02b2c0581f628f23999981f497403af0dc3c0967
jesus <jibanez@tacitknowledge.com>
2018-03-07 14:56:46 -0600

    at unit testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -55,6 +56,7 @@ export const firebaseConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyA3GGV-O_vrweaT7J_i-jKDRikYPki11rQ'
--- src/app/components/lugares/lugares.component.ts
+++ src/app/components/lugares/lugares.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../../services/lugares.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'lugares',
   templateUrl: './lugares.component.html',
-  styleUrls:['./lugares.component.css']
+  styleUrls:['./lugares.component.css'],
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0,
+        //backgroundColor: 'green',
+        //transform: 'rotate3d(0,0,0,0deg)',
+      })),
+        state('final', style({
+          opacity: 1,
+          //backgroundColor: 'yellow',
+          //transform: 'rotate3d(5,10,20,30deg)'
+        })),
+        transition('inicial => final', animate(1000)),
+        transition('final => inicial', animate(650)),
+    ])
+  ],
 })
 export class LugaresComponent {
     public lat: number = 24.7828658;
     public lng: number = -107.400825;
     public lugares:any;
+    public state:string = 'inicial';
+
+    animar() {
+      this.state = (this.state==='final')?'inicial':'final';
+    }
+
+    animacionInicial(e) {
+      console.log("inicio");
+    }
+
+    animacionTerminal(e) {
+      console.log("terminado");
+    }
     constructor(private _lugaresService:LugaresService) {
       this._lugaresService.getLugares()
       //.valueChanges()
@@ -17,6 +47,7 @@ export class LugaresComponent {
         lugares => {
             this.lugares = Object.keys(lugares).map(function(key){return lugares[key]});
             console.log(this.lugares);
+            this.state = 'final';
         },
         error => {
           console.log(error);
